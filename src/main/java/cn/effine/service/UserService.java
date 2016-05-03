e80874b1763fa9d2e9abfe3c94e6dd25cd208948
effine <effine@effine-mac.lan>
2016-05-03 23:05:10 +0800

    create file

---
+++ src/main/java/cn/effine/service/UserService.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * @author  effine | iballader#gmail.com
+ * @email   iballader#gmail.com
+ * @date    2016年5月3日  下午10:59:15
+ * @site    http://effine.cn
+ * @since   1.0
+ */
+
+package cn.effine.service;
+
+public class UserService {
+
+}
+
+
---
+++ src/main/java/cn/effine/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+/**
+ * @author  effine | iballader#gmail.com
+ * @email   iballader#gmail.com
+ * @date    2016年5月3日  下午11:04:14
+ * @site    http://effine.cn
+ * @since   1.0
+ */
+
+package cn.effine.service.impl;
+
+public class UserServiceImpl {
+
+}
+
+
